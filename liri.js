7a4317b02fb19b4bd39ccdcbf0084fb1e00ade15
Annie <anniebeltran23@gmail.com>
2018-10-19 21:20:56 -0700

    all files

---
+++ liri.js
@@ -0,0 +1,19 @@
+js
+require("dotenv").config();
+
+/* 8. Add the code required to import the `keys.js` file and store it in a variable.
+  
+* You should then be able to access your keys information like so
+
+  ```js
+  var spotify = new Spotify(keys.spotify); */
+
+  /*9. Make it so liri.js can take in one of the following commands:
+
+   * `concert-this`
+
+   * `spotify-this-song`
+
+   * `movie-this`
+
+   * `do-what-it-says`
